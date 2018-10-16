293538c5272b23d78e37dac9c3a6f31a839db45e
Yuro <yuluo1203@gmail.com>
2018-10-16 01:13:47 -0400

    add a springboot-demo1

---
+++ springboot-demo1/src/main/java/com/yuro/springboot/springbootdemo1/SpringbootDemo1Application.java
@@ -0,0 +1,12 @@
+package com.yuro.springboot.springbootdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemo1Application.class, args);
+    }
+}
